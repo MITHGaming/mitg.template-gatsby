a95dfced320723e25e0e466d7a14ccc938099451
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-03-25 14:45:38 -0400

    refactor: change metod to import fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,31 @@
 /* eslint-disable react/no-danger */
 import * as React from 'react';
+import fs from 'fs';
 import { getCssText } from './src/stitches.config';
 import ThemeLayoutProvider from './src/hooks/useThemeLayoutProvider';
 import LayoutWrapper from './src/layout';
+import { globalStyles } from './src/styles/globalCss';
 
-export const onRenderBody = ({ setHeadComponents }) => {
+const getAllFonts = () => {
+  const dir = fs.readdirSync('./static/fonts');
+  return dir.filter((file) => file.endsWith('.woff2'));
+};
+
+export const onRenderBody = async ({ setHeadComponents }) => {
+  setHeadComponents([
+    getAllFonts().map((font) => (
+      <link
+        key={font}
+        rel="preload"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
+        href={`/fonts/${font}`}
+      />
+    )),
+  ]);
+  globalStyles();
+
   setHeadComponents([
     <style
       key="stitches"
--- src/styles/globalCss.ts
+++ src/styles/globalCss.ts
@@ -3,36 +3,37 @@ import './resets/_modern-normalize.css';
 import './resets/reset.css';
 
 export const globalStyles = globalCss({
-  /* '@font-face': [
+  '@font-face': [
     {
       fontFamily: `Poppins`,
       fontWeight: `300`,
-      src: `local(""), url("../fonts/Poppins-Light.ttf") format("truetype")`,
+      src: `local(""), url("/fonts/Poppins-Light.woff2") format("woff2")`,
     },
     {
       fontFamily: `Poppins`,
       fontWeight: `400`,
-      src: `local(""), url("../fonts/Poppins-Regular.ttf") format("truetype")`,
+      src: `local(""), url("../fonts/Poppins-Regular.woff2") format("woff2")`,
     },
     {
       fontFamily: `Poppins`,
       fontWeight: `500`,
-      src: `local(""), url("../fonts/Poppins-Medium.ttf") format("truetype")`,
+      src: `local(""), url("/fonts/Poppins-Medium.woff2") format("woff2")`,
     },
     {
       fontFamily: `Poppins`,
       fontWeight: `600`,
-      src: `local(""), url("../fonts/Poppins-SemiBold.ttf") format("truetype")`,
+      src: `local(""), url("/fonts/Poppins-SemiBold.woff2") format("woff2")`,
     },
     {
       fontFamily: `Poppins`,
       fontWeight: `700`,
-      src: `local(""), url("../fonts/Poppins-Bold.ttf") format("truetype")`,
+      src: `local(""), url("/fonts/Poppins-Bold.woff2") format("woff2")`,
     },
-  ], */
+  ],
   '*': {
     fontFamily: `Poppins`,
     fontSize: `16px`,
     fontWeight: `400`,
+    fontDisplay: `swap`,
   },
 });
