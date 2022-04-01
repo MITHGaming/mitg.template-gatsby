e287990e506f813dcf4088a7c3bde0a778b5fca7
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-03-31 21:17:33 -0400

    refactor: change info config and add logo mitg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `MITG Template Gatsby`,
+    description: `É um template para a utilização para os próximos projetos em gatsby`,
+    author: `@kamity`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mitg-template-gatsby`,
+        short_name: `mitg`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FFB923`,
+        theme_color: `#FFB923`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mitg-icon.svg`, // This path is relative to the root of the site.
       },
     },
   ],
