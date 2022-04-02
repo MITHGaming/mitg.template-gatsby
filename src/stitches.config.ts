819db0a58265653f69a062c402392a431a4b999d
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-04-02 00:37:22 -0400

    feat: add conotation before each class css generated by stitches

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -3,6 +3,7 @@ import type * as Stitches from '@stitches/react';
 
 export type CSS = Stitches.CSS<typeof config>;
 export const { styled, css, getCssText, config, createTheme } = createStitches({
+  prefix: `mitg`,
   theme: {
     colors: {
       // Project Colors
